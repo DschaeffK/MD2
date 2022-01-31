0e0912fd3bffc12deca3a3a9cff5c3ebc5ca8143
Jeffrey King <jk95@hotmail.de>
2022-01-31 19:46:39 +0100

    final

--- script.js
+++ script.js
@@ -22,6 +22,17 @@ let wolkenOrigins = [];
 let trigger_first_info = document.getElementById('trigger-first-info');
 let first_info = document.getElementById('first-info');
 
+let trigger_second_info = document.getElementById('trigger-second-info');
+let second_info = document.getElementById('second-info');
+
+let trigger_third_info = document.getElementById('trigger-third-info');
+let third_info = document.getElementById('third-info');
+
+let trigger_fourth_info = document.getElementById('trigger-fourth-info');
+let fourth_info = document.getElementById('fourth-info');
+
+let flieger = document.getElementById('flieger');
+
 for(let wolke of wolken) {
     wolkenOrigins[wolke.id] = getOrigin(wolke);
 }
@@ -42,35 +53,27 @@ window.addEventListener('scroll', function() {
         }
     }
 
-    console.log("trigger-top: " + trigger_first_info.getBoundingClientRect().top);
-    console.log("trigger-bottom: " + trigger_first_info.getBoundingClientRect().bottom);
-    console.log("apfel: " + (apfel.getBoundingClientRect().top + apfel.getBoundingClientRect().top / 2));
     let apfelCenter = apfel.getBoundingClientRect().top + apfel.getBoundingClientRect().top / 2;
-    if (apfelCenter > trigger_first_info.getBoundingClientRect().top - 100 && apfelCenter < trigger_first_info.getBoundingClientRect().bottom + 100) {
-        first_info.classList.remove('fade-out-left');
-        first_info.classList.add('fade-in-left');
-    } else {
-        first_info.classList.add('fade-out-left');
-        first_info.classList.remove('fade-in-left');
-    }
+    fadeInFirstFact(apfelCenter);
+    fadeInSecondFact(apfelCenter);
+    fadeInThirdFact(apfelCenter);
+    fadeInFourthFact(apfelCenter);
 
     // title.style.color = 'rgba(255,255,255,' + (100 - (scrollPercentage * 18)) / 100 + ')';
-    if(scrollY > 4825) {
-        apfelTransforms[0] = 'translateY(' + (scrollY - 4830 /*(3680 + 4400)*/) * 1.15 + 'px)';
-        apfelTransforms[1] = 'rotate(' + ((scrollPercentage - 34) * 4.8) + 'deg)';
+    if(scrollY > 4735) {
+        apfelTransforms[0] = 'translateY(' + (scrollY - 4735 /*(3680 + 4400)*/) * 1.15 + 'px)';
+        apfelTransforms[1] = 'rotate(' + ((scrollPercentage - 42) * 4.8) + 'deg)';
         apfel.style.transform = apfelTransforms.join(' ');
     }
 
-    if (scrollY > 12000) {
+    if (scrollY > 10000) {
         apfel.style.display = 'none';
     } else {
         apfel.style.display = 'inline-block';
     }
 
-    // console.log('scrollY: ' + scrollY);
-    // console.log('ast Position: ' + astOrigin + scrollY * 2);
-    // console.log('fullheight: ' + fullHeight);
-    // console.log('scrollPercentage: ' + scrollPercentage);
+    flieger.style.transform = 'translateX(' + (scrollY - 8000) + 'px) translateY('+ (scrollY - 8000) * 0.6 +'px)';
+
     freezeElement(sonne, scrollY - 4200);
 
     for(let wolke of wolken) {
@@ -78,6 +81,50 @@ window.addEventListener('scroll', function() {
     }
     // continueScrolling(apfel, scrollY, 1.05);
 });
+
+function fadeInFirstFact(apfel)
+{
+    if (apfel > trigger_first_info.getBoundingClientRect().top - 100 && apfel < trigger_first_info.getBoundingClientRect().bottom + 100) {
+        first_info.classList.remove('fade-out-left-1');
+        first_info.classList.add('fade-in-left-1');
+    } else {
+        first_info.classList.add('fade-out-left-1');
+        first_info.classList.remove('fade-in-left-1');
+    }
+}
+
+function fadeInSecondFact(apfel)
+{
+    if (apfel > trigger_second_info.getBoundingClientRect().top - 50 && apfel < trigger_second_info.getBoundingClientRect().bottom + 150) {
+        second_info.classList.remove('fade-out-right-2');
+        second_info.classList.add('fade-in-right-2');
+    } else {
+        second_info.classList.add('fade-out-right-2');
+        second_info.classList.remove('fade-in-right-2');
+    }
+}
+
+function fadeInThirdFact(apfel)
+{
+    if (apfel > trigger_third_info.getBoundingClientRect().top - 20 && apfel < trigger_third_info.getBoundingClientRect().bottom + 200) {
+        third_info.classList.remove('fade-out-left-3');
+        third_info.classList.add('fade-in-left-3');
+    } else {
+        third_info.classList.add('fade-out-left-3');
+        third_info.classList.remove('fade-in-left-3');
+    }
+}
+
+function fadeInFourthFact(apfel)
+{
+    if (apfel > trigger_fourth_info.getBoundingClientRect().top + 50 && apfel < trigger_fourth_info.getBoundingClientRect().bottom + 250) {
+        fourth_info.classList.remove('fade-out-right-4');
+        fourth_info.classList.add('fade-in-right-4');
+    } else {
+        fourth_info.classList.add('fade-out-right-4');
+        fourth_info.classList.remove('fade-in-right-4');
+    }
+}
 
 function freezeElement(element, scrollY)
 {
